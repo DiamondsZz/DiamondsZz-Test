37a7d53118bc4ea30ca61cbffe5d3548e3e25993
DiamondsZz <DiamondsZzZz@163.com>
2020-11-03 18:39:36 +0800

    v-html  v-on

--- vueTest/minivue/js/vue.js
+++ vueTest/minivue/js/vue.js
@@ -1,33 +1,47 @@
 class Vue {
-  constructor (options) {
+  constructor(options) {
     // 1. 通过属性保存选项的数据
-    this.$options = options || {}
-    this.$data = options.data || {}
-    this.$el = typeof options.el === 'string' ? document.querySelector(options.el) : options.el
+    this.$options = options || {};
+    this.$data = options.data || {};
+    this.$el =
+      typeof options.el === "string"
+        ? document.querySelector(options.el)
+        : options.el;
+
+    //转换方法
+    this._transformMethods(options.methods || []);
     // 2. 把data中的成员转换成getter和setter，注入到vue实例中
-    this._proxyData(this.$data)
+    this._proxyData(this.$data);
     // 3. 调用observer对象，监听数据的变化
-    new Observer(this.$data)
+    new Observer(this.$data);
     // 4. 调用compiler对象，解析指令和差值表达式
-    new Compiler(this)
+    new Compiler(this);
   }
-  _proxyData (data) {
+  _proxyData(data) {
     // 遍历data中的所有属性
-    Object.keys(data).forEach(key => {
+    Object.keys(data).forEach((key) => {
       // 把data的属性注入到vue实例中
       Object.defineProperty(this, key, {
         enumerable: true,
         configurable: true,
-        get () {
-          return data[key]
+        get() {
+          return data[key];
         },
-        set (newValue) {
+        set(newValue) {
           if (newValue === data[key]) {
-            return
+            return;
           }
-          data[key] = newValue
-        }
-      })
-    })
+          data[key] = newValue;
+        },
+      });
+    });
+  }
+  //转换方法
+  _transformMethods(data) {
+    if (Object.keys(data).length > 0) {
+      for (let item of Object.keys(data)) {
+        this[item] = data[item];
+      }
+    }
   }
 }
