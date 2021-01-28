b22cf8b96fc92a72ebaf6dfee40d591245abc828
DiamondsZz <DiamondsZzZz@163.com>
2021-01-28 15:09:20 +0800

    redux-test

--- react-test/src/components/App/App.js
+++ react-test/src/components/App/App.js
@@ -1,5 +1,35 @@
-function App() {
-  return <div className="App">run</div>;
+import React from "react";
+import { createStore } from "redux";
+
+function reducer(state = { value: 0 }, action) {
+  switch (action.type) {
+    case "add":
+      return { value: state.value + 1 };
+    default:
+      return state;
+  }
+}
+const store = createStore(reducer);
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = store.getState();
+    this.add = this.add.bind(this);
+  }
+  add() {
+    store.dispatch({ type: "add" });
+    this.setState(store.getState());
+  }
+  render() {
+    return (
+      <div className="App">
+        <button>- </button>
+        {this.state.value}
+        <button onClick={this.add}>+ </button>
+      </div>
+    );
+  }
 }
 
 export default App;
