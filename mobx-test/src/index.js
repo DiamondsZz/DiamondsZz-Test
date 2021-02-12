cca167dcbe5b40d8b91eba43500ce4fe7cdbea8f
Diamonds <DiamondsZzZz@163.com>
2021-02-12 12:39:57 +0800

    test

--- mobx-test/src/index.js
+++ mobx-test/src/index.js
@@ -1,4 +1,6 @@
 // import index from './index_redux';
 // import index from './index_mobx';
 // import userMobx from './user-mobx';
-import "./user-redux";
+import "./user-mobx";
+import './styles/appleBasket.scss';
+import './styles/appleItem.scss';
