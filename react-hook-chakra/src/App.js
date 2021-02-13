b63a55e4ecc368f7323fd046a0541eadc027b491
Diamonds <DiamondsZzZz@163.com>
2021-02-13 21:06:21 +0800

    test-init

--- react-hook-chakra/src/App.js
+++ react-hook-chakra/src/App.js
@@ -1,8 +1,35 @@
-import { Button, ButtonGroup } from "@chakra-ui/react"
-
-
+import {
+  Box,
+  Tabs,
+  Tab,
+  TabList,
+  TabPanels,
+  TabPanel,
+  Text,
+} from "@chakra-ui/react";
+import Signin from "./Signin";
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Box bgColor="gray.200" h="100vh" py="100px">
+      <Box w={500} mx="auto" bgColor="white" borderRadius="md">
+        <Tabs isFitted>
+          <TabList px={150}>
+            <Tab _focus={{ boxShadow: "none" }}>登录</Tab>
+            <Text display="flex" alignItems="center" mx="20px">
+              ·
+            </Text>
+            <Tab _focus={{ boxShadow: "none" }}>注册</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Signin />
+            </TabPanel>
+            <TabPanel>2</TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Box>
+  );
 }
 
 export default App;
---
+++ react-hook-chakra/src/Signin.js
@@ -0,0 +1,10 @@
+import { Box, Button, Input } from "@chakra-ui/react";
+export default function Signin(props) {
+  return (
+    <Box>
+      <Input />
+      <Input />
+      <Button>登录</Button>
+    </Box>
+  );
+}
