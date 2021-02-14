62fb266c0c39779ef9d563720285db25994442c6
Diamonds <DiamondsZzZz@163.com>
2021-02-14 19:45:21 +0800

    js

--- react-hook-chakra/src/Signin.js
+++ react-hook-chakra/src/Signin.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import {
   Box,
   Button,
@@ -8,16 +10,96 @@ import {
   Checkbox,
   Text,
   Divider,
-  HStack 
+  HStack,
+  useToast,
 } from "@chakra-ui/react";
 import { PhoneIcon, LockIcon } from "@chakra-ui/icons";
 import { AiOutlineQq, AiOutlineWeibo, AiOutlineWechat } from "react-icons/ai";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+function tip(toast) {
+  toast({
+    description: "敬请期待！！！",
+    status: "warning",
+    duration: 1000,
+    isClosable: true,
+    position: "top",
+  });
+}
+
 export default function Signin(props) {
+  const [loading, setLoading] = useState(false);
+  const toast = useToast();
+  const formik = useFormik({
+    initialValues: {
+      phoneOrEmail: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      const { phoneOrEmail, password } = values;
+      if (!phoneOrEmail || !phoneOrEmail.trim()) {
+        return toast({
+          description: "请输入手机号或邮箱",
+          status: "error",
+          duration: 2000,
+          isClosable: true,
+          position: "top",
+        });
+      }
+      if (!password || !password.trim()) {
+        return toast({
+          description: "请输入密码",
+          status: "error",
+          duration: 2000,
+          isClosable: true,
+          position: "top",
+        });
+      }
+      setLoading(true);
+      fetch("https://conduit.productionready.io/api/users/login", {
+        method: "POST",
+        body: JSON.stringify({
+          user: {
+            email: phoneOrEmail,
+            password,
+          },
+        }),
+      })
+        .then((res) => {
+          if (res.status === 200) return res.json();
+          else {
+            setLoading(false);
+            toast({
+              description: "登录失败",
+              status: "error",
+              duration: 2000,
+              isClosable: true,
+              position: "top",
+            });
+          }
+        })
+        .then((res) => {
+          if (res) {
+            setLoading(false);
+            toast({
+              description: "登录成功",
+              status: "success",
+              duration: 2000,
+              isClosable: true,
+              position: "top",
+            });
+          }
+        });
+    },
+  });
+
   return (
     <Box mt="30px">
       <InputGroup>
         <InputLeftElement children={<PhoneIcon color="gray.300" />} />
         <Input
+          name="phoneOrEmail"
           variant="filled"
           placeholder="手机号或邮箱"
           borderBottom="none"
@@ -26,11 +108,13 @@ export default function Signin(props) {
           borderWidth="1px"
           focusBorderColor="#c8c8c8"
           bgColor="#b5b5b51a"
+          {...formik.getFieldProps("phoneOrEmail")}
         />
       </InputGroup>
       <InputGroup>
         <InputLeftElement children={<LockIcon color="gray.300" />} />
         <Input
+          name="password"
           variant="filled"
           placeholder="密码"
           borderTopRadius="0px"
@@ -38,13 +122,19 @@ export default function Signin(props) {
           borderWidth="1px"
           focusBorderColor="#c8c8c8"
           bgColor="#b5b5b51a"
+          {...formik.getFieldProps("password")}
         />
       </InputGroup>
       <Flex justify="space-between" my="18px">
         <Checkbox defaultIsChecked color="#969696" size="sm">
           记住我
         </Checkbox>
-        <Text color="#969696" fontSize="14px" _hover={{cursor:'pointer'}}>
+        <Text
+          color="#969696"
+          fontSize="14px"
+          _hover={{ cursor: "pointer" }}
+          onClick={() => tip(toast)}
+        >
           登录遇到问题？
         </Text>
       </Flex>
@@ -54,6 +144,8 @@ export default function Signin(props) {
         color="#fff"
         bgColor="blue.300"
         _hover={{ bgColor: "blue.500" }}
+        onClick={formik.handleSubmit}
+        isLoading={loading}
       >
         登录
       </Button>
@@ -64,11 +156,26 @@ export default function Signin(props) {
         </Text>
         <Divider />
       </Flex>
-      <HStack  mt="16px" px="20px" spacing="24px" justify="center">
-        <AiOutlineWeibo fontSize="28px" color="#e05244" cursor="pointer"/>
-        <AiOutlineWechat fontSize="28px" color="#00bb29" cursor="pointer" />
-        <AiOutlineQq fontSize="28px" color="#498ad5" cursor="pointer" />
-      </HStack >
+      <HStack mt="16px" px="20px" spacing="24px" justify="center">
+        <AiOutlineWeibo
+          fontSize="28px"
+          color="#e05244"
+          cursor="pointer"
+          onClick={() => tip(toast)}
+        />
+        <AiOutlineWechat
+          fontSize="28px"
+          color="#00bb29"
+          cursor="pointer"
+          onClick={() => tip(toast)}
+        />
+        <AiOutlineQq
+          fontSize="28px"
+          color="#498ad5"
+          cursor="pointer"
+          onClick={() => tip(toast)}
+        />
+      </HStack>
     </Box>
   );
 }
--- react-hook-chakra/src/Signup.js
+++ react-hook-chakra/src/Signup.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import {
   Box,
   Button,
@@ -7,16 +9,107 @@ import {
   Divider,
   HStack,
   Flex,
-  Text
+  Text,
+  useToast,
 } from "@chakra-ui/react";
 import { PhoneIcon, LockIcon, AtSignIcon } from "@chakra-ui/icons";
 import { AiOutlineQq, AiOutlineWechat } from "react-icons/ai";
+import { useFormik } from "formik";
+
+function tip(toast) {
+  toast({
+    description: "敬请期待！！！",
+    duration: 1000,
+    isClosable: true,
+    position: "top",
+  });
+}
+
 export default function Signin(props) {
+  const [loading, setLoading] = useState(false);
+
+  const toast = useToast();
+
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      phone: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      const { username, phone, password } = values;
+      if (!username || !username.trim()) {
+        return toast({
+          description: "请输入你的昵称",
+          status: "error",
+          duration: 2000,
+          isClosable: true,
+          position: "top",
+        });
+      }
+      if (!phone || !phone.trim()) {
+        return toast({
+          description: "请输入手机号",
+          status: "error",
+          duration: 2000,
+          isClosable: true,
+          position: "top",
+        });
+      }
+      if (!password || !password.trim()) {
+        return toast({
+          description: "请输入密码",
+          status: "error",
+          duration: 2000,
+          isClosable: true,
+          position: "top",
+        });
+      }
+      setLoading(true);
+      fetch(" https://conduit.productionready.io/api/users", {
+        method: "POST",
+        body: JSON.stringify({
+          user: {
+            username,
+            email: phone,
+            password,
+          },
+        }),
+      })
+        .then((res) => {
+          if (res.status === 200) return res.json();
+          else {
+            setLoading(false);
+            toast({
+              description: "注册失败",
+              status: "error",
+              duration: 2000,
+              isClosable: true,
+              position: "top",
+            });
+          }
+        })
+        .then((res) => {
+          if (res) {
+            setLoading(false);
+            toast({
+              description: "注册成功",
+              status: "success",
+              duration: 2000,
+              isClosable: true,
+              position: "top",
+            });
+          }
+        });
+    },
+  });
+
   return (
     <Box mt="30px">
       <InputGroup>
         <InputLeftElement children={<AtSignIcon color="gray.300" />} />
         <Input
+          name="username"
           variant="filled"
           placeholder="你的昵称"
           borderBottom="none"
@@ -25,11 +118,13 @@ export default function Signin(props) {
           borderWidth="1px"
           focusBorderColor="#c8c8c8"
           bgColor="#b5b5b51a"
+          {...formik.getFieldProps("username")}
         />
       </InputGroup>
       <InputGroup>
         <InputLeftElement children={<PhoneIcon color="gray.300" />} />
         <Input
+          name="phone"
           variant="filled"
           placeholder="手机号"
           borderBottom="none"
@@ -39,11 +134,13 @@ export default function Signin(props) {
           borderWidth="1px"
           focusBorderColor="#c8c8c8"
           bgColor="#b5b5b51a"
+          {...formik.getFieldProps("phone")}
         />
       </InputGroup>
       <InputGroup>
         <InputLeftElement children={<LockIcon color="gray.300" />} />
         <Input
+          name="password"
           variant="filled"
           placeholder="密码"
           borderTopRadius="0px"
@@ -51,6 +148,7 @@ export default function Signin(props) {
           borderWidth="1px"
           focusBorderColor="#c8c8c8"
           bgColor="#b5b5b51a"
+          {...formik.getFieldProps("password")}
         />
       </InputGroup>
 
@@ -61,7 +159,8 @@ export default function Signin(props) {
         bgColor="#42c02e"
         my="18px"
         _hover={{ bgColor: "green" }}
-        isLoading
+        onClick={formik.handleSubmit}
+        isLoading={loading}
       >
         注册
       </Button>
@@ -73,8 +172,18 @@ export default function Signin(props) {
         <Divider />
       </Flex>
       <HStack mt="16px" px="20px" spacing="24px" justify="center">
-        <AiOutlineWechat fontSize="28px" color="#00bb29" cursor="pointer"/>
-        <AiOutlineQq fontSize="28px" color="#498ad5" cursor="pointer"/>
+        <AiOutlineWechat
+          fontSize="28px"
+          color="#00bb29"
+          cursor="pointer"
+          onClick={() => tip(toast)}
+        />
+        <AiOutlineQq
+          fontSize="28px"
+          color="#498ad5"
+          cursor="pointer"
+          onClick={() => tip(toast)}
+        />
       </HStack>
     </Box>
   );
