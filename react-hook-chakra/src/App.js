d3d2317a422aac32d949c1d808ff403a648515f5
Diamonds <DiamondsZzZz@163.com>
2021-02-14 17:50:21 +0800

    view

--- react-hook-chakra/src/App.js
+++ react-hook-chakra/src/App.js
@@ -8,23 +8,45 @@ import {
   Text,
 } from "@chakra-ui/react";
 import Signin from "./Signin";
+import Signup from "./Signup";
 function App() {
   return (
     <Box bgColor="#f1f1f1" h="100vh" py="100px">
-      <Box w={400} mx="auto"  py={10}  bgColor="white" borderRadius="md" boxShadow="md">
+      <Box
+        w={400}
+        mx="auto"
+        py={10}
+        bgColor="white"
+        borderRadius="md"
+        boxShadow="md"
+      >
         <Tabs isFitted colorScheme="red" size="lg">
-          <TabList px='100px' borderBottom	={'none'}>
-            <Tab _focus={{ boxShadow: "none"}} _active={{ backgroundColor: "transparent"}}  color="#969696" >登录</Tab>
+          <TabList px="100px" borderBottom={"none"}>
+            <Tab
+              _focus={{ boxShadow: "none" }}
+              _active={{ backgroundColor: "transparent" }}
+              color="#969696"
+            >
+              登录
+            </Tab>
             <Text display="flex" alignItems="center" mx="20px">
               ·
             </Text>
-            <Tab _focus={{ boxShadow: "none"}} _active={{ backgroundColor: "transparent"}}   color="#969696">注册</Tab>
+            <Tab
+              _focus={{ boxShadow: "none" }}
+              _active={{ backgroundColor: "transparent" }}
+              color="#969696"
+            >
+              注册
+            </Tab>
           </TabList>
           <TabPanels px="30px">
             <TabPanel>
               <Signin />
             </TabPanel>
-            <TabPanel>2</TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
           </TabPanels>
         </Tabs>
       </Box>
--- react-hook-chakra/src/Signup.js
+++ react-hook-chakra/src/Signup.js
@@ -4,24 +4,37 @@ import {
   InputGroup,
   Input,
   InputLeftElement,
-  Flex,
-  Checkbox,
-  Text,
   Divider,
-  HStack 
+  HStack,
+  Flex,
+  Text
 } from "@chakra-ui/react";
-import { PhoneIcon, LockIcon } from "@chakra-ui/icons";
-import { AiOutlineQq, AiOutlineWeibo, AiOutlineWechat } from "react-icons/ai";
+import { PhoneIcon, LockIcon, AtSignIcon } from "@chakra-ui/icons";
+import { AiOutlineQq, AiOutlineWechat } from "react-icons/ai";
 export default function Signin(props) {
   return (
     <Box mt="30px">
       <InputGroup>
+        <InputLeftElement children={<AtSignIcon color="gray.300" />} />
+        <Input
+          variant="filled"
+          placeholder="你的昵称"
+          borderBottom="none"
+          borderBottomRadius="0px"
+          borderColor="#c8c8c8"
+          borderWidth="1px"
+          focusBorderColor="#c8c8c8"
+          bgColor="#b5b5b51a"
+        />
+      </InputGroup>
+      <InputGroup>
         <InputLeftElement children={<PhoneIcon color="gray.300" />} />
         <Input
           variant="filled"
-          placeholder="手机号或邮箱"
+          placeholder="手机号"
           borderBottom="none"
           borderBottomRadius="0px"
+          borderTopRadius="0px"
           borderColor="#c8c8c8"
           borderWidth="1px"
           focusBorderColor="#c8c8c8"
@@ -40,35 +53,29 @@ export default function Signin(props) {
           bgColor="#b5b5b51a"
         />
       </InputGroup>
-      <Flex justify="space-between" my="18px">
-        <Checkbox defaultIsChecked color="#969696" size="sm">
-          记住我
-        </Checkbox>
-        <Text color="#969696" fontSize="14px" _hover={{cursor:'pointer'}}>
-          登录遇到问题？
-        </Text>
-      </Flex>
+
       <Button
         w="100%"
         borderRadius={24}
         color="#fff"
-        bgColor="blue.300"
-        _hover={{ bgColor: "blue.500" }}
+        bgColor="#42c02e"
+        my="18px"
+        _hover={{ bgColor: "green" }}
+        isLoading
       >
-        登录
+        注册
       </Button>
-      <Flex mt="30px" align="center" px="20px">
+      <Flex mt="12px" align="center" px="20px">
         <Divider />
         <Text flexShrink={0} mx="20px" fontSize="12px" color="#b5b5b5">
           社交账号登录
         </Text>
         <Divider />
       </Flex>
-      <HStack  mt="16px" px="20px" spacing="24px" justify="center">
-        <AiOutlineWeibo fontSize="28px" color="#e05244" cursor="pointer"/>
-        <AiOutlineWechat fontSize="28px" color="#00bb29" cursor="pointer" />
-        <AiOutlineQq fontSize="28px" color="#498ad5" cursor="pointer" />
-      </HStack >
+      <HStack mt="16px" px="20px" spacing="24px" justify="center">
+        <AiOutlineWechat fontSize="28px" color="#00bb29" cursor="pointer"/>
+        <AiOutlineQq fontSize="28px" color="#498ad5" cursor="pointer"/>
+      </HStack>
     </Box>
   );
 }
