c0ef179303a4fb3a13b4b0a672cf9ca214b008ed
DiamondsZz <DiamondsZzZz@163.com>
2021-03-26 14:31:02 +0800

    拷贝

--- interviewTest/index.js
+++ interviewTest/index.js
@@ -3,18 +3,18 @@
  * 要求实现去访问目标对象example中不存在的属性时，抛出错误：Property “$(property)” does not exist    （2018 今日头条）
  */
 
-// const man = {
-//   name: "jscoder",
-//   age: 23,
-// };
+const man = {
+  name: "jscoder",
+  age: 23,
+};
 
-// const manProxy = new Proxy(man, {
-//   //拦截对象属性的读取
-//   get(target, propKey) {
-//     if (target[propKey]) return target[propKey];
-//     else return `property ${propKey} does not exist`
-//   },
-// });
+const manProxy = new Proxy(man, {
+  //拦截对象属性的读取
+  get(target, propKey) {
+    if (target[propKey]) return target[propKey];
+    else return `property ${propKey} does not exist`;
+  },
+});
 // console.log(manProxy.name);
 // console.log(manProxy.age);
 // console.log(manProxy.location);
@@ -67,21 +67,21 @@ function greenYellowRed() {
  * 3.
  */
 
-// var User = {
-//   count: 1,
-//   action: {
-//     getCount: function () {
-//       //console.log(this);
-//       return this.count;
-//     },
-//   },
-// };
+var User = {
+  count: 1,
+  action: {
+    getCount: function () {
+      //console.log(this);
+      return this.count;
+    },
+  },
+};
 
-// var getCount = User.action.getCount;
+var getCount = User.action.getCount;
 
-// setTimeout(() => {
-//   console.log("result1", User.action.getCount());
-// },2000);
+setTimeout(() => {
+  console.log("result1", User.action.getCount());
+}, 2000);
 
 // console.log("result2", getCount());
 
@@ -107,6 +107,89 @@ function change(o) {
 
 let b = change(a);
 
-console.log(b.age); // 第一个console   30
+// console.log(b.age); // 第一个console   30
 
-console.log(a.age); // 第二个console   24
+// console.log(a.age); // 第二个console   24
+
+/**
+ *
+ * 5.
+ */
+/* 对象的拷贝 */
+
+const isComplexDataType = (obj) =>
+  (typeof obj === "object" || typeof obj === "function") && obj !== null;
+
+const deepClone = function (obj, hash = new WeakMap()) {
+  if (obj.constructor === Date) return new Date(obj); // 日期对象直接返回一个新的日期对象
+
+  if (obj.constructor === RegExp) return new RegExp(obj); //正则对象直接返回一个新的正则对象
+
+  //如果循环引用了就用 weakMap 来解决
+
+  if (hash.has(obj)) return hash.get(obj);
+
+  let allDesc = Object.getOwnPropertyDescriptors(obj);
+
+  //遍历传入参数所有键的特性
+
+  let cloneObj = Object.create(Object.getPrototypeOf(obj), allDesc);
+
+  //继承原型链
+
+  hash.set(obj, cloneObj);
+
+  for (let key of Reflect.ownKeys(obj)) {
+    cloneObj[key] =
+      isComplexDataType(obj[key]) && typeof obj[key] !== "function"
+        ? deepClone(obj[key], hash)
+        : obj[key];
+  }
+
+  return cloneObj;
+};
+
+// 下面是验证代码
+
+let obj = {
+  num: 0,
+
+  str: "",
+
+  boolean: true,
+
+  unf: undefined,
+
+  nul: null,
+
+  obj: { name: "我是一个对象", id: 1 },
+
+  arr: [0, 1, 2],
+
+  func: function () {
+    console.log("我是一个函数");
+  },
+
+  date: new Date(0),
+
+  reg: new RegExp("/我是一个正则/ig"),
+
+  [Symbol("1")]: 1,
+};
+
+Object.defineProperty(obj, "innumerable", {
+  enumerable: false,
+  value: "不可枚举属性",
+});
+
+obj = Object.create(obj, Object.getOwnPropertyDescriptors(obj));
+
+obj.loop = obj; // 设置loop成循环引用的属性
+
+let cloneObj = deepClone(obj);
+
+cloneObj.arr.push(4);
+
+// console.log("obj", obj);
+
+// console.log("cloneObj", cloneObj);
