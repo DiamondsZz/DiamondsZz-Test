57227f6b5ae900a0a70d9f35f886dbf7748c8218
DiamondsZz <DiamondsZzZz@163.com>
2021-04-06 16:36:45 +0800

    宏任务、微任务

--- interviewTest/index.js
+++ interviewTest/index.js
@@ -566,3 +566,35 @@ function jsonStringify(data) {
 }
 
 //console.log(JSON.stringify([2,5,'ggg']));
+
+/**
+ * 任务
+ */
+
+// async function async1() {
+//   console.log("async1 start");
+
+//   await async2();
+
+//   console.log("async1 end");
+// }
+
+// async function async2() {
+//   console.log("async2");
+// }
+
+// async1();
+
+// setTimeout(() => {
+//   console.log("timeout");
+// }, 0);
+
+// new Promise(function (resolve) {
+//   console.log("promise1");
+
+//   resolve();
+// }).then(function () {
+//   console.log("promise2");
+// });
+
+// console.log("script end");
