f12bb2fdec0b7a72d2879492e40c895dfd5e528e
Diamonds <DiamondsZzZz@163.com>
2021-04-07 21:35:27 +0800

    update

--- interviewTest/index.js
+++ interviewTest/index.js
@@ -598,3 +598,25 @@ function jsonStringify(data) {
 // });
 
 // console.log("script end");
+
+function* gen() {
+  console.log("enter");
+
+  let a = yield 1;
+  let b = yield (function () {
+    return 2;
+  })();
+
+  return 3;
+}
+
+var g = gen(); // 阻塞住，不会执行任何语句
+
+// console.log(g.next());
+
+// console.log(g.next());
+
+// console.log(g.next());
+
+// console.log(g.next())
+// console.log(g.next())
